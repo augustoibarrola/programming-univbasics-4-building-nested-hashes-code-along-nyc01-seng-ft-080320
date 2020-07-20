aa069d41c72036f6aff672163e961e15aea30cf7
Augusto Ibarrola <augusto.t.ibarrola@gmail.com>
2020-07-20 18:34:25 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -2,27 +2,35 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        #your key/value pairs here
+        :name => "Lord Montague",
+        :age => "53"
         },
       :matriarch => {
-        #your key/value pairs here
+        :name => "Lady Montague",
+        :age => "54"
         },
       :hero => {
-        #your key/value pairs here
+        :name => "Romeo",
+        :age => "15"
+        :status => "Alive"
         },
-      :hero_friends => []
+      :hero_friends => [:Benvolio, :Mercutio]
    }, 
    :capulet => {
       :patriarch => {
-        #your key/value pairs here
+        :name => "Lord Capulet",
+        :age => "50" 
         },
       :matriarch => {
-        #your key/value pairs here
+        :name => "Lady Capulet",
+        :age => "51" 
         },
       :heroine => {
-        #your key/value pairs here
+        :name => "Juliet",
+        :age => "15",
+        :status => "Alive" 
         },
-      :heroine_friends => []
+      :heroine_friends => [:Steven, :Nurse]
    }
   }
 
